a1a0e7d9816aafba4bd1f3018929753b29c663f5
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2022-08-30 01:28:21 +0200

    Add server argument to /alert

--- src/main/java/net/earthmc/velocitycommands/commands/AlertCommand.java
+++ src/main/java/net/earthmc/velocitycommands/commands/AlertCommand.java
@@ -1,11 +1,17 @@
 package net.earthmc.velocitycommands.commands;
 
 import com.velocitypowered.api.command.SimpleCommand;
-import com.velocitypowered.api.proxy.ConsoleCommandSource;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class AlertCommand extends BaseCommand implements SimpleCommand {
     private final ProxyServer proxy;
@@ -21,16 +27,40 @@ public class AlertCommand extends BaseCommand implements SimpleCommand {
             return;
         }
 
-        if (invocation.arguments().length == 0) {
-            invocation.source().sendMessage(Component.text("Not enough arguments! Usage: /alert [message]", NamedTextColor.RED));
+        if (invocation.arguments().length < 2) {
+            invocation.source().sendMessage(Component.text("Not enough arguments! Usage: /alert [server(s)|all] [message]", NamedTextColor.RED));
             return;
         }
 
-        Component message = MiniMessage.miniMessage().deserialize(String.join(" ", invocation.arguments()));
-        proxy.sendMessage(message);
+        Audience recipients;
+        if ("all".equalsIgnoreCase(invocation.arguments()[0]))
+            recipients = proxy;
+        else {
+            Set<RegisteredServer> servers = new HashSet<>();
+            Set<String> invalidServers = new HashSet<>();
 
-        if (invocation.source() instanceof ConsoleCommandSource)
-            invocation.source().sendMessage(message);
+            for (String server : invocation.arguments()[0].split(",")) {
+                Optional<RegisteredServer> optServer = proxy.getServer(server);
+
+                if (optServer.isEmpty())
+                    invalidServers.add(server);
+                else
+                    servers.add(optServer.get());
+            }
+
+            if (!invalidServers.isEmpty())
+                invocation.source().sendMessage(Component.text("The following servers are invalid: " + String.join(", ", invalidServers) + ".", NamedTextColor.GOLD));
+
+            if (!servers.isEmpty())
+                invocation.source().sendMessage(Component.text("Broadcasting message to " + servers.size() + " server" + (servers.size() == 1 ? "" : "s") + ".", NamedTextColor.GOLD));
+            else
+                return;
+
+            recipients = Audience.audience(servers);
+        }
+
+        String[] messageArgs = Arrays.copyOfRange(invocation.arguments(), 1, invocation.arguments().length);
+        recipients.sendMessage(MiniMessage.miniMessage().deserialize(String.join(" ", messageArgs)));
     }
 
     @Override
